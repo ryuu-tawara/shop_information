6a696644fdf650da64116b1245dff04b7ff201f8
tawara <tawara19981206@outlook.jp>
2023-11-11 16:33:06 +0900

    fix_js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-$.getJSON("https://github.com/ryuu-tawara/shop_information/blob/main/shop_information.json", function(json) {
+$.getJSON("./shop_information.json", function(json) {
     console.log(json); 
 });
